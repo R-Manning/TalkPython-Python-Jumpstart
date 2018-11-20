2ab73859ecf2791b36454d663491f56937b04358
Ryan M <43008568+R-Manning@users.noreply.github.com>
2018-11-20 14:56:18 -0500

    Add files via upload
    initial commit of the guess the number game:
    
    randomly generate an integer from 0-100
    ask user to guess the number
    compare the number, give feedback
    end when the user guesses the correct number

---
+++ Guess_That_Number_game_program.py
@@ -0,0 +1,21 @@
+from random import randint
+
+print('--------------------------------')
+print('     Guess That Number Game     ')
+print('--------------------------------')
+
+rand_num = randint(0, 100)
+guess = -1
+num_of_guesses = 0
+
+while rand_num != guess:
+    num_of_guesses += 1
+    guess = int(input('Guess a number between 0 and 100: '))
+
+    if guess == rand_num:
+        print('You guessed the number {0} correctly!')
+    elif guess < rand_num:
+        print('The number is greater than your guess of {0}.')
+    else:
+        print('The number is less than your guess of {0}.')
+
