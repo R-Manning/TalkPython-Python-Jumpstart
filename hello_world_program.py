22755620ae9fb00d2ca0cafdb43b4192fabd0050
Ryan M <43008568+R-Manning@users.noreply.github.com>
2018-11-20 13:54:07 -0500

    Add files via upload
    committing code for hello_world

---
+++ hello_world_program.py
@@ -0,0 +1,11 @@
+#Printing program header with end of line character
+print('-------------------------\n')
+print('       Hello App\n')
+print('-------------------------\n')
+
+#Ask user for name, save input and add to greeting
+name = input('What is your name? ')
+greeting = 'Nice to meet you ' + name
+
+#print the greeting to the user
+print(greeting)
