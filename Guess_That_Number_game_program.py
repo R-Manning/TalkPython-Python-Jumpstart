c4a299c63fc9e5db3fdcaf8367597fdd28f93e8b
Ryan M <43008568+R-Manning@users.noreply.github.com>
2018-11-20 15:20:21 -0500

    Update Guess_That_Number_game_program.py
    updated the code so that the feedback on the conditionals actually works
    
    added a print statement to tell the user how many turns it took them to guess the number correctly

--- Guess_That_Number_game_program.py
+++ Guess_That_Number_game_program.py
@@ -13,9 +13,9 @@ while rand_num != guess:
     guess = int(input('Guess a number between 0 and 100: '))
 
     if guess == rand_num:
-        print('You guessed the number {0} correctly!')
+        print('You guessed the number {0} correctly!'.format(guess))
+        print('\nIt took you {0} turns.'.format(num_of_guesses))
     elif guess < rand_num:
-        print('The number is greater than your guess of {0}.')
+        print('The number is greater than your guess of {0}.'.format(guess))
     else:
-        print('The number is less than your guess of {0}.')
-
+        print('The number is less than your guess of {0}.'.format(guess))
