ce0957f2052a08a76943ab8d092e302355c4b7bf
Ryan M <43008568+R-Manning@users.noreply.github.com>
2018-11-20 19:26:37 -0500

    Update Guess_That_Number_game_program.py
    got rid of "0's" in the format braces, un-needed
    
    capitalized the feedback to make it more obvious
    
    added to-dos

--- Guess_That_Number_game_program.py
+++ Guess_That_Number_game_program.py
@@ -13,9 +13,13 @@ while rand_num != guess:
     guess = int(input('Guess a number between 0 and 100: '))
 
     if guess == rand_num:
-        print('You guessed the number {0} correctly!'.format(guess))
-        print('\nIt took you {0} turns.'.format(num_of_guesses))
+        print('You guessed the number {} correctly!'.format(guess))
+        print('\nIt took you {} turns.'.format(num_of_guesses))
     elif guess < rand_num:
-        print('The number is greater than your guess of {0}.'.format(guess))
+        print('The number is GREATER than your guess of {}.'.format(guess))
     else:
-        print('The number is less than your guess of {0}.'.format(guess))
+        print('The number is LESS than your guess of {}.'.format(guess))
+#TODO: save "high-score", lowest number of guesses to get the correct answer
+#program should also load the "high-score" and compare it to the user's score at the end of the game
+#TODO: add user-names, save by user-names, add database of usernames, highscores overall/by user
+#other add-ons that you can think of
